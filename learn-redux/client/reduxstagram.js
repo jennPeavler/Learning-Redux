7529f09f3887fa3edcc6d2b607c7cf559963c9ca
Jennifer Peavler <jenn.peavler@gmail.com>
2017-04-23 11:49:47 -0600

    finish learning sentry

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,28 @@
-// let's go!
+import React from 'react'
+import { render } from 'react-dom'
+import css from './styles/style.styl'
+import App from './components/App'
+import PhotoGrid from './components/PhotoGrid'
+import Single from './components/Single'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
+import store, { history } from './store'
+import Raven from 'raven-js'
+import { sentry_url } from './data/config'
+
+Raven.config(sentry_url).install()
+
+console.log(window.doesNotExist.nope)
+
+const router = (
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+
+    </Router>
+  </Provider>
+)
+render(router, document.getElementById('root'))
